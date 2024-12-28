6540d445914443cc85912b85b6dda6c29a596072
Sebastián Campos Rojas <sebastian94.scr@gmail.com>
2024-12-28 17:58:25 +0100

    primer commit trabajo de enfoque desarrollo de interfaces

---
+++ src/main/java/com/mycompany/trabajo_enfoque_desarrollo_de_interfaces/Trabajo_enfoque_desarrollo_de_interfaces.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo_enfoque_desarrollo_de_interfaces;
+
+/**
+ *
+ * @author sebastiancamposrojas
+ */
+public class Trabajo_enfoque_desarrollo_de_interfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a SmartOcupation.");
+        
+        SmartOcupationApp smartOcupation = new SmartOcupationApp();
+        smartOcupation.setVisible(true);
+    }
+}
